08ef450437fbfcb48520c44963f3e92666fdfa05
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-26 22:01:29 +1000

    Well, it communicates...

--- haskell/dilemma.hs
+++ haskell/dilemma.hs
@@ -3,10 +3,15 @@
 module Main where
 
 import System.ZMQ4.Monadic
-import Data.List.NonEmpty (fromList)
 import Control.Monad
+import Data.List.NonEmpty (fromList)
 import Data.ByteString.Char8 (unpack)
 import Text.Printf
+
+respond ("ping":_) = ["pong"]
+respond ("reset":_) = ["ok"]
+respond ("iterate":a) = ["C"]
+respond _ = ["error"]
 
 main :: IO ()
 main =
@@ -14,7 +19,8 @@ main =
         requester <- socket Req
         connect requester "tcp://localhost:1441"
 
-        sendMulti requester (fromList ["reg", "hs:test:1"])
+        sendMulti requester (fromList ["reg", "hs:sample"])
 
-        msg <- receiveMulti requester
-        liftIO $ printf "Received reply %s\n" (unpack (head msg))
+        forever $ do
+          msg <- receiveMulti requester
+          sendMulti requester (fromList (respond msg))
