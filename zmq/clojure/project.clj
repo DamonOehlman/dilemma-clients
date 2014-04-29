3a22ce7e4da6b7d62f34375a357d0f203668d0c0
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-30 08:24:06 +1000

    Removed clojure client - not implemented

--- zmq/clojure/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject dilemma-example "0.0.0-SNAPSHOT"
-  :description "Simple dilemma example"
-  :jvm-opts ["-Djava.library.path=/usr/lib:/usr/local/lib"]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.zeromq/cljzmq "0.1.5-SNAPSHOT"]]
-  :main dilemma.sample)
